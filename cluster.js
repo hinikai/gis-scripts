330fe46864c7fc8c732f75de6c042e25c101eab8
MOMO <momo@MOMOdeMacBook-Air.local>
2016-01-27 22:48:24 +0800

    nikai

---
+++ cluster.js
@@ -0,0 +1,1 @@
+var projection = require('./projection');
---
+++ projection.js
@@ -0,0 +1,26 @@
+//经纬度转墨卡托
+function lngLat2Mercator(lnglat) {
+    var x = lnglat.x * 20037508.34 / 180;
+    var y = Math.log(Math.tan((90 + lnglat.y) * Math.PI / 360)) / (Math.PI / 180);
+    y = y * 20037508.34 / 180;
+    return {
+        x: x,
+        y: y
+    };
+}
+
+//墨卡托转经纬度
+function mercator2LngLat(mercator) {
+    var x = mercator.x / 20037508.34 * 180;
+    var y = mercator.y / 20037508.34 * 180;
+    y = 180 / Math.PI * (2 * Math.atan(Math.exp(y * Math.PI / 180)) - Math.PI / 2);
+    return {
+        x: x,
+        y: y
+    };
+}
+
+module.exports = {
+	lngLat2Mercator: lngLat2Mercator,
+    mercator2LngLat: mercator2LngLat
+}
